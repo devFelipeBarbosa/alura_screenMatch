fd54c508ebc5b8b9f022f49dc94351d37152cbdb
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2025-07-11 22:50:27 -0300

    Criando métodos que modificam valores

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,23 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anodeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnodeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anodeLancamento);
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
 
     }
+    https://cursos.alura.com.br/course/java-aplicando-orientacao-objetos/task/125821
 }
+
